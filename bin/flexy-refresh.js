be5873993fa67439293ceb3b95aaf53a7bcdf74f
blackmoja <limlim980625@gmail.com>
2022-09-19 19:06:41 +0900

    feat: flexy-refresh init

---
+++ bin/flexy-refresh.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+console.log('👋 - welcome to flexy-refresh')
+console.log('👋 - flexy-refresh is a tool to refresh the flexy server')
+console.log('👋 - We will support the function soon!')
